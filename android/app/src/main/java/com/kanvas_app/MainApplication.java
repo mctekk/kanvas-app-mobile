8a28327a7fcdb3e1768c4ee1d99a44f2949d21fc
Jose Andre Ceron <jose@mctekk.com>
2024-04-30 14:22:34 -0400

    Feat: facebook social login on android

--- android/app/src/main/java/com/kanvas_app/MainApplication.java
+++ android/app/src/main/java/com/kanvas_app/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import com.facebook.FacebookSdk;
 
 public class MainApplication extends Application implements ReactApplication {
 

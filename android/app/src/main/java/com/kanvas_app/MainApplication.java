3c47ab7cd3ea509cb756050f0ba450b73f9563ee
Jose Andre Ceron <jose@mctekk.com>
2024-04-30 13:48:33 -0400

    Feat: facebook social login and env icons added on iOS

--- android/app/src/main/java/com/kanvas_app/MainApplication.java
+++ android/app/src/main/java/com/kanvas_app/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
 
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+            new FBSDKPackage();
           return packages;
         }
 

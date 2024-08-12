01cd076e2e5136dce26299a4dcaa66ced3a0a30a
Jose Andre Ceron <jose@mctekk.com>
2024-08-12 16:04:56 -0400

    Refactor: Upgrating RN versions

--- babel.config.js
+++ babel.config.js
@@ -36,7 +36,7 @@ module.exports = function (api) {
     }
   }
   return {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['module:@react-native/babel-preset'],
     plugins,
   };
 };
